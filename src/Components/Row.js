edb6884571c11d207c8f89746aa5a791589005c1
otmane1993 <otmomks@gmail.com>
2021-11-15 21:37:45 +0000

    Voici le livrable de la journée avec sweet alert

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import Boutton from './Boutton';
 import '../index.css';
+import swal from 'sweetalert';
 const Row = (props) => {
     const removeItem=(e)=>{
-        e.target.parentElement.parentElement.remove();
-        // console.log(e.target.value)
+        swal({
+            title: "Are you sure?",
+            text: "Do you want to delete the row",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+            e.target.parentElement.parentElement.remove();
+              swal("The row has been deleted!", {
+                icon: "success",
+              });
+            } else {
+              swal("The row is live");
+            }
+          });
+        // e.target.parentElement.parentElement.remove();
     }
     return (
         <>
